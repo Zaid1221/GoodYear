99296d769c4f482bee9f5fb9f99f18330136f841
Zaid1221 <zaid.javaid@gmail.com>
2017-10-04 02:01:27 -0400

    Should be fixed

--- app/src/main/java/com/example/zaid/goodyear/MainActivity.java
+++ app/src/main/java/com/example/zaid/goodyear/MainActivity.java
@@ -24,9 +24,11 @@ import java.util.Scanner;
 public class MainActivity extends AppCompatActivity {
 
     private ProgressBar spinner;
-    private String userInput;
-    private EditText TermtoAnalyze;
+    private String article;
+    private TextView dateFrom;
+    private TextView dateTo;
     private TextView Result;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,37 +38,86 @@ public class MainActivity extends AppCompatActivity {
         spinner = (ProgressBar) findViewById(R.id.progressBar);
         spinner.setVisibility(View.GONE);
 
-        TermtoAnalyze = (EditText) findViewById(R.id.TermtoAnalyze);
+        dateFrom = (TextView) findViewById(R.id.DateFrom);
+        dateTo = (TextView) findViewById(R.id.DateTo);
         Result = (TextView) findViewById(R.id.result);
-    }
-
-    public void searchMethod(View v) {
-        Uri builtUri2 = Uri.parse("https://api.meaningcloud.com/sentiment-2.1").buildUpon()
-                .appendQueryParameter("key", "5cefecfee765dde6c9c943db4c891c88")
-                .appendQueryParameter("txt", TermtoAnalyze.getText().toString())
-                .appendQueryParameter("lang", "en")
-                .build();
-        new FindSentimentTask().execute(builtUri2.toString());
-        spinner.setVisibility(View.VISIBLE);
 
         Calendar c = Calendar.getInstance();
         System.out.println("Current time => " + c.getTime());
-
         SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd");
         String formattedDate = df.format(c.getTime());
         //get one year ago
         c.add(Calendar.YEAR, -1);
         String yearAgoDate = df.format(c.getTime());
 
-        Uri builtUri3 = Uri.parse("https://api.nytimes.com/svc/search/v2/articlesearch.json").buildUpon()
-                .appendQueryParameter("api-key", "4bc349cc561c4e5d8e7023eff1bf7019")
-                .appendQueryParameter("begin_date", yearAgoDate)
-                .appendQueryParameter("end_date", formattedDate)
+        dateFrom.setText(yearAgoDate);
+        dateTo.setText(formattedDate);
+    }
+
+    public void searchMethod(View v) {
+        Uri builtUri = Uri.parse("https://api.nytimes.com/svc/search/v2/articlesearch.json").buildUpon()
+                .appendQueryParameter("api-key", "5cefecfee765dde6c9c943db4c891c88")
+                .appendQueryParameter("begin_date", dateFrom.getText().toString())
+                .appendQueryParameter("end_date", dateTo.getText().toString())
                 .build();
-        new yearEvaluation().execute(builtUri3.toString());
+        new yearEvaluation().execute(builtUri.toString());
+
+        Uri builtUri2 = Uri.parse("https://api.meaningcloud.com/sentiment-2.1").buildUpon()
+                .appendQueryParameter("api-key", "4bc349cc561c4e5d8e7023eff1bf7019")
+                .appendQueryParameter("txt", article)
+                .appendQueryParameter("lang", "en")
+                .build();
+        new FindSentimentTask().execute(builtUri2.toString());
         spinner.setVisibility(View.VISIBLE);
     }
 
+
+    public class yearEvaluation extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... url) {
+            //android.os.Debug.waitForDebugger();
+            String toreturn = "Did not work";
+            try {
+                toreturn = getResponseFromHttpUrl(url[0]);
+            } catch (Exception e) {
+                Log.d("ErrorInApp", "exception on get Response from HTTP call" + e.getMessage());
+            }
+            return toreturn;
+        }
+
+        protected void onPostExecute(String sentimentData) {
+            try {
+                JSONObject Jarticle = new JSONObject(sentimentData);
+                JSONObject answer = Jarticle.getJSONObject("response");
+                JSONArray Jarray = answer.getJSONArray("docs");
+                JSONObject index;
+
+                String temp = "";
+                for(int i=0; i< Jarray.length(); i++){
+                    index = Jarray.getJSONObject(i);
+                    temp += index.getString("headline");
+                }
+
+                String[] headlines = temp.split("\"main\":\"");
+                String ALLHEADLINES ="";
+
+                for(int i = 1; i < headlines.length; i++)
+                {
+                    String[] headlines2 = headlines[i].split("\"");
+                    ALLHEADLINES += headlines2[0].toString() + " ";
+                }
+                //Result.setText(ALLHEADLINES);
+                article = ALLHEADLINES; //Set Variable to Be Analyzed in the Sentiment API
+
+
+                spinner.setVisibility(View.GONE);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            super.onPostExecute(sentimentData);
+        }
+    }
 
     public class FindSentimentTask extends AsyncTask<String, Void, String> {
         @Override
@@ -100,43 +151,6 @@ public class MainActivity extends AppCompatActivity {
 
 
                 Result.setText("Score Tag: " + scoreTag + "\n" + "Confidence: " + confidenceTag + "\n" + "Irony: " + ironyTag);
-                spinner.setVisibility(View.GONE);
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            super.onPostExecute(sentimentData);
-        }
-    }
-
-
-    public class yearEvaluation extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... url) {
-            android.os.Debug.waitForDebugger();
-            String toreturn = "Did not work";
-            try {
-                toreturn = getResponseFromHttpUrl(url[0]);
-            } catch (Exception e) {
-                Log.d("ErrorInApp", "exception on get Response from HTTP call" + e.getMessage());
-            }
-            return toreturn;
-        }
-
-        protected void onPostExecute(String sentimentData) {
-            try {
-                JSONObject sentimentJSON = new JSONObject(sentimentData);
-
-                JSONObject answer = sentimentJSON.getJSONObject("response");
-                JSONArray Jarray = answer.getJSONArray("docs");
-                JSONObject index = Jarray.getJSONObject(0);
-
-                for(int i=0; i< Jarray.length(); i++){
-                    index = Jarray.getJSONObject(i);
-                    sentimentData = index.toString();
-                }
-
-                TermtoAnalyze.setText(sentimentData);
                 spinner.setVisibility(View.GONE);
 
             } catch (Exception e) {
